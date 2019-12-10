1a9b804a8c27a7bc0116206bfd2562a5c1a25504
DEVASENA <43780390+DEVASENA999@users.noreply.github.com>
2019-12-11 00:46:15 +0530

    Add files via upload

---
+++ P1/P1.java
@@ -0,0 +1,7 @@
+public class P1 {
+	public static void main(String[] args) {
+		new Fruit().eat();
+		new Apple().eat();
+		new Orange().eat();
+	}
+}
---
+++ P2/P2.java
@@ -0,0 +1,13 @@
+public class P2 {
+	public static void main(String[] args) {
+		Shape c = new Circle();
+		Shape t = new Triangle();
+		Shape s = new Square();
+		c.draw();
+		c.erase();
+		t.draw();
+		t.erase();
+		s.draw();
+		s.erase();
+	}
+}
